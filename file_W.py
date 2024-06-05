8d695820fec042d98045f7c5b86b760e38ef6766
AlexandraO <alexponomareva@mail.ru>
2024-06-05 09:27:02 +0300

    добавлена функция переноса строки в другой файл

--- file_W.py
+++ file_W.py
@@ -4,9 +4,9 @@ def work_with_phonebook():
     choice = show_menu()
     phone_book = read_txt('phonebook.txt') #буферная переменная, которая из функции рид возвратила список в списке - наш справочник
     #print(phone_book)
-    while (choice != 7):
+    while (choice != 8):
         if choice == 1:
-            print_result(phone_book)
+            print_result('phonebook.txt')
         elif choice == 2:
             #last_name = input('Введите фамилию абонента ')
             #print(find_user(phone_book,last_name,0))
@@ -21,13 +21,34 @@ def work_with_phonebook():
         elif choice==5:
             print(write_txt('phonebook.txt',phone_book))
         elif choice==6:
+             copy_phone_line(phone_book,'phonebook_new.txt')           
+        elif choice == 7:            
             choice = out_confirmation()
+
         choice=show_menu()
 
-def print_result(phone_book):
-    with open('phonebook.txt', 'r', encoding='utf-8') as phb:
+#Дополнить справочник возможностью копирования данных из одного файла в другой. 
+#Пользователь вводит номер строки, которую необходимо перенести из одного файла в другой.
+
+def copy_phone_line(phone_book,filename_new):
+    number_copy = int(input("Введите номер строки копирования: ")) - 1
+    for i in range(len(phone_book)):
+        if i == number_copy:
+            with open(filename_new,'w',encoding='utf-8') as phout_new:
+                s = phone_book[i]
+                #for v in phone_book[i].values():
+                #    s = s + v + ','
+                #phone_book.append(s)
+                phout_new.write(f'{s}\n')
+                print('Строка', i+1 , 'записана v новый файл') 
+
+
+def print_result(filename):
+    with open(filename, 'r', encoding='utf-8') as phb:
         for line in phb:
             print(line, end='')
+
+# другая версия поиска (одна функция на оба поиска с флагом)
 
 # def find_user(phone_book, value, flag):
 #     if flag == 0: print("Ищем по фамилии")
@@ -42,7 +63,7 @@ def print_result(phone_book):
 #     return res
 
 def find_lastname(phone_book):
-    last_name = input ("VVedi lastname : ")
+    last_name = input ("Введите фамилию : ")
     res = "---------------\n"
     for i in range(len(phone_book)):
         if phone_book[i]["ФАМИЛИЯ"] == last_name:
@@ -52,7 +73,7 @@ def find_lastname(phone_book):
     return res
 
 def find_number(phone_book):
-    phone_name = input ("VVedi phone : ")
+    phone_name = input ("Введите номер : ")
     res = "---------------\n"
     for i in range(len(phone_book)):
         if phone_book[i]["ТЕЛЕФОН"] == phone_name:
@@ -69,12 +90,13 @@ def add_user(phone_book):
     return record
 
 def write_txt(filename, phone_book):
-    with open('phonebook.txt','w',encoding='utf-8') as phout:
+    with open(filename,'w',encoding='utf-8') as phout:
         for i in range(len(phone_book)):
             s=''
             for v in phone_book[i].values():
                 s = s + v + ','
             phout.write(f'{s[:-1]}\n')
+    print("Справочник перезаписан")
 
 def out_confirmation():
     print("Закрыть?")
@@ -103,7 +125,8 @@ def show_menu():
           "3. Найти по номеру телефона\n"
           "4. Добавить абонента в справочник\n"
           "5. Сохранить справочник в тхт\n"
-          "6. Закончить работу")
+          "6. Записать абонента вдругой файл\n"
+          "7. Закончить работу")
     choice = int(input())
     return choice
 
